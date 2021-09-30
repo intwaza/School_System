36c4dd27c59585326f2ad199857bb4697c1023d8
intwaza <iintwazabelyse@gmail.com>
2021-09-30 09:39:05 +0300

    add defaults

--- trainer/models.py
+++ trainer/models.py
@@ -2,30 +2,30 @@ from django.db import models
 
 class Trainer(models.Model):
     first_name= models.CharField(
-        max_length=12
+        max_length=12, default='SOME STRING'
     )
     last_name = models.CharField(
-        max_length=12
+        max_length=12, default='SOME STRING'
     )
-    age = models.PositiveSmallIntegerField()
+    age = models.PositiveSmallIntegerField(default='SOME STRING')
     gender_choice=(
         ('1','Female'),
         ('2','male'),
         ('3','none')
     )
     gender = models.CharField(
-        max_length=8, choices=gender_choice
+        max_length=8, choices=gender_choice, default='SOME STRING'
     )
     bio= models.TextField(
-        max_length=700
+        max_length=700, default='SOME STRING'
     )
     email_address= models.EmailField()
     phone_number = models.CharField(
-        max_length=12
+        max_length=12, default='SOME STRING'
     )
-    salary= models.PositiveBigIntegerField()
-    profile= models.ImageField(upload_to ='images/')
-    contract= models.FileField(upload_to='documents/')
-    date_hired= models.DateField()
+    salary= models.PositiveBigIntegerField(default='SOME STRING')
+    profile= models.ImageField(upload_to ='images/', default='SOME STRING')
+    contract= models.FileField(upload_to='documents/', default='SOME STRING')
+    date_hired= models.DateField(default='SOME STRING')
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
