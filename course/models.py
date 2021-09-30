51bc6b3b269b4170a623710573b7ba88dc05a863
intwaza <iintwazabelyse@gmail.com>
2021-09-30 09:17:30 +0300

    add defaults

--- course/models.py
+++ course/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 class Course(models.Model):
     course_name= models.CharField(
-        max_length=20
+        max_length=20, default='SOME STRING'
     )
     course_trainer= models.CharField(
-        max_length=12
+        max_length=12, default='SOME STRING'
     )
     course_code= models.CharField(
-        max_length=12
+        max_length=12, default='SOME STRING'
     )
     course_description= models.FileField(upload_to='documents/')
-    course_duration= models.PositiveSmallIntegerField()
+    course_duration= models.PositiveSmallIntegerField(default='SOME STRING')
     syllabus= models.TextField(
-        max_length=700
+        max_length=700, default='SOME STRING'
     )
 
